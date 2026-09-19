ab7605472c46d6e46c86f3dbab33f16d8e4840d0
KIRVAS <kirvas1989@mail.ru>
2026-09-19 13:21:47 +0300

    feat(code): separate WealthMeter start value and Poor threshold in config

--- ButchersGames/Assets/_Project/Scripts/Core/Gameplay/WealthMeter/Api/IWealthMeterSettings.cs
+++ ButchersGames/Assets/_Project/Scripts/Core/Gameplay/WealthMeter/Api/IWealthMeterSettings.cs
@@ -3,6 +3,7 @@ namespace Core.Gameplay.WealthMeter
     public interface IWealthMeterSettings
     {
         int StartValue { get; }
+        int PoorThreshold { get; }
         int DescentThreshold { get; }
         int CasualThreshold { get; }
         int RichThreshold { get; }
--- ButchersGames/Assets/_Project/Scripts/ViewComponents/WealthMeter/Api/WealthMeterConfig.cs
+++ ButchersGames/Assets/_Project/Scripts/ViewComponents/WealthMeter/Api/WealthMeterConfig.cs
@@ -10,12 +10,14 @@ namespace ViewComponents.WealthMeter
           IWealthMeterSettings
     {
         [SerializeField] private int _startValue;
+        [SerializeField] private int _poorThreshold;
         [SerializeField] private int _descentThreshold;
         [SerializeField] private int _casualThreshold;
         [SerializeField] private int _richThreshold;
         [SerializeField] private int _millionaireThreshold;
 
         public int StartValue => _startValue;
+        public int PoorThreshold => _poorThreshold;
         public int DescentThreshold => _descentThreshold;
         public int CasualThreshold => _casualThreshold;
         public int RichThreshold => _richThreshold;
@@ -23,31 +25,16 @@ namespace ViewComponents.WealthMeter
 
         public void Validate()
         {
-            Guard.AgainstNonPositive(_startValue, () => new InvalidWealthMeterValueException(nameof(_startValue), _startValue));
+            Guard.AgainstNegative(_startValue, () => Invalid(nameof(_startValue), _startValue));
+            Guard.AgainstNegative(_poorThreshold, () => Invalid(nameof(_poorThreshold), _poorThreshold));
+            Guard.AgainstGreaterThan(_poorThreshold, _descentThreshold - 1, () => Invalid(nameof(_poorThreshold), _poorThreshold));
+            Guard.AgainstGreaterThan(_descentThreshold, _casualThreshold - 1, () => Invalid(nameof(_descentThreshold), _descentThreshold));
+            Guard.AgainstGreaterThan(_casualThreshold, _richThreshold - 1, () => Invalid(nameof(_casualThreshold), _casualThreshold));
+            Guard.AgainstGreaterThan(_richThreshold, _millionaireThreshold - 1, () => Invalid(nameof(_richThreshold), _richThreshold));
 
-            Guard.AgainstGreaterThan(
-                _startValue,
-                _descentThreshold - 1,
-                () => new InvalidWealthMeterValueException(nameof(_startValue), _startValue)
-            );
+            return;
 
-            Guard.AgainstGreaterThan(
-                _descentThreshold,
-                _casualThreshold - 1,
-                () => new InvalidWealthMeterValueException(nameof(_descentThreshold), _descentThreshold)
-            );
-
-            Guard.AgainstGreaterThan(
-                _casualThreshold,
-                _richThreshold - 1,
-                () => new InvalidWealthMeterValueException(nameof(_casualThreshold), _casualThreshold)
-            );
-
-            Guard.AgainstGreaterThan(
-                _richThreshold,
-                _millionaireThreshold - 1,
-                () => new InvalidWealthMeterValueException(nameof(_richThreshold), _richThreshold)
-            );
+            ExtendedException Invalid(string fieldName, int value) => new InvalidWealthMeterValueException(fieldName, value);
         }
     }
 }
